0cceb71915f8900cf569c84f6147ff47ca83b884
zhangaifei <zhangaifei@51.com>
2018-12-21 16:20:16 +0800

    update about page

--- src/demo/src/views/About.vue
+++ src/demo/src/views/About.vue
@@ -1,3 +1,39 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <div class="md-example-child md-example-child-result-page md-example-child-result-page-2">
+    <md-result-page
+            :buttons="buttons">
+    </md-result-page>
+  </div>
 </template>
+
+<script>
+export default {
+    name: 'result-page-demo',
+    data() {
+        return {
+            buttons: [
+                {
+                    text: '普通按钮',
+                    handler() {
+                        Toast.succeed('普通操作')
+                    },
+                },
+                {
+                    text: '高亮按钮',
+                    type: 'ghost-primary',
+                    handler() {
+                        Toast.succeed('不普通操作')
+                    },
+                },
+            ],
+        }
+    },
+}
+
+</script>
+
+<style>
+  .md-result-page.customized img{
+    height: 131px;
+  }
+</style>
